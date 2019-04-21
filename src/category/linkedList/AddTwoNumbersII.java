4b2261ffa626fc59e7b64e5820aa6b12fe8a8a0a
brianzhang <willwillzhang@gmail.com>
2019-04-21 13:53:29 -0400

    update

---
+++ src/category/linkedList/AddTwoNumbersII.java
@@ -0,0 +1,64 @@
+package category.linkedList;
+
+/**
+ * Solution: reverse the linkedlist + carry the number
+ * stack can be another solution
+ */
+public class AddTwoNumbersII {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+
+        ListNode rel1 = reverse(l1);
+        ListNode rel2 = reverse(l2);
+
+        ListNode res = new ListNode(0);
+        ListNode currP = res;
+
+        int carry = 0;
+        while (rel1 != null || rel2 != null) {
+            int sum = carry + (rel1 == null ? 0 : rel1.val) + (rel2 == null ? 0 : rel2.val);
+            carry = sum / 10;
+            int digit = sum % 10;
+            rel1 = (rel1 == null ? null : rel1.next);
+            rel2 = (rel2 == null ? null : rel2.next);
+            ListNode curr = new ListNode(digit);
+            currP.next = curr;
+            currP = curr;
+        }
+
+        if (carry != 0) {
+            ListNode curr = new ListNode(carry);
+            currP.next = curr;
+        }
+
+        return reverse(res.next);
+
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/category/linkedList/PalindromeLinkedList.java
@@ -0,0 +1,43 @@
+package category.linkedList;
+
+/**
+ * Explanation
+ * Example :
+   1-> 2-> 3-> 4-> 2-> 1
+
+    ref points 1 initially.
+    Make recursive calls until you reach the last element - 1.
+    On returning from each recurssion, check if it is equal to ref values. 
+    ref values are updated to on each recurssion.
+    So first check is ref 1 -  end 1
+    Second ref 2 - second last 2 ...and so on.
+ * 
+ */
+public class PalindromeLinkedList{
+
+    ListNode ref;
+    public boolean isPalindrome(ListNode head) {
+        if(head == null)
+            return true;
+       ref = head; 
+       return dfs(head);
+    }
+    
+    public boolean dfs(ListNode node){
+        if(node == null)
+            return true;
+        
+        boolean res = dfs(node.next);
+        
+        if(!res){
+            return false;
+        }
+        if(ref.val == node.val){
+            ref = ref.next;
+        }else{
+            return false;
+        }
+        
+        return true;
+    }
+}

0f3a56635cf0ccbeb7bbbd14d08936acb398b352
brianzhang <willwillzhang@gmail.com>
2019-04-21 13:50:40 -0400

    update

---
+++ src/category/tree/trie/LinkedList/BoundaryOfBinaryTree.java
@@ -0,0 +1,52 @@
+package category.tree.trie.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import amazon.model.TreeNode;
+
+public class BoundaryOfBinaryTree{
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        // write your code here
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null)
+            return res;
+         
+        res.add(root.val);  
+        collectLeft(root.left, res);
+        collectLeaves(root, res);
+        collectRight(root.right, res);
+        return res;    
+    }
+    
+    //pre-order traversal
+    public void collectLeft(TreeNode root, List<Integer> res){
+        if(root == null || isLeaf(root))
+            return;
+        res.add(root.val);
+        collectLeft(root.left !=null ? root.left : root.right, res);
+    }
+    //postorder traversal
+    public void collectRight(TreeNode root, List<Integer> res){
+        if(root == null || isLeaf(root))
+            return;
+        
+        collectRight(root.right != null ? root.right : root.left, res);
+        res.add(root.val);
+    }
+    
+    public void collectLeaves(TreeNode root, List<Integer> res){
+        if(root == null)
+            return;
+        if(isLeaf(root)){
+            res.add(root.val);
+            return;
+        }else{
+             collectLeaves(root.left, res);
+             collectLeaves(root.right, res);
+        }
+    }
+    
+    private boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}

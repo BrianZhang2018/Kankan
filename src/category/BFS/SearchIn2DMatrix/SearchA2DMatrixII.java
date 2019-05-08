265a114c7d0ce2ade1d96534d5bb671ba44597e3
brianzhang <willwillzhang@gmail.com>
2019-05-07 23:49:28 -0400

    update

---
+++ src/category/BFS/SearchIn2DMatrix/SearchA2DMatrixII.java
@@ -0,0 +1,52 @@
+package category.BFS.SearchIn2DMatrix;
+/**
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/
+ * BFS的经典用法，可以用在很多其他的问题中去寻找一个element在2D matrix中
+ * 而且是最快的方法，快于DFS
+ */
+public class SearchA2DMatrixII{
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix == null || matrix.length == 0)
+            return false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                
+                if(matrix[i][j] == target){
+                    return true;
+                }else{
+                    if(bfs(matrix, i, j, target))
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean bfs(int[][] matrix, int i, int j, int target){
+        int[][] dires = new int[][]{{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for(int[] dire : dires){
+            int nl = i + dire[0];
+            int nr = j + dire[1];
+            
+            if(nl>m-1 || nl<0 || nr>n-1 || nr<0 || matrix[nl][nr] < target){
+                continue;
+            }
+            
+            if(matrix[nl][nr] == target){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}

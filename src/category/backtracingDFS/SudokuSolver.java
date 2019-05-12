e90e7eb85982fb3eedfd749687462cfb6333e938
brianzhang <willwillzhang@gmail.com>
2019-05-12 01:08:17 -0400

    update

---
+++ src/category/backtracingDFS/SudokuSolver.java
@@ -0,0 +1,40 @@
+
+package category.backtracingDFS;
+
+/**
+ * https://leetcode.com/problems/sudoku-solver/
+ */
+public class SudokuSolver{
+
+    public void solveSudoku(char[][] board) {
+        dfs(board, 0);
+    }
+    //返回值不是void的backtracking, compare with SubsetsII and Permutation
+    public boolean dfs(char[][] board, int d) {
+        if(d==81)
+            return true;
+        int i=d/9, j=d%9; // get the index of next cell
+        if (board[i][j] != '.')
+            return dfs(board, d+1);
+        
+        for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, i, j, c)) {
+                board[i][j] = c;
+                if (dfs(board, d+1)) //move to next cell 
+                    return true;
+            }
+        }
+        board[i][j] = '.'; //reset the status
+        return false;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+            if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == c) return false;
+        }
+        return true;
+    }
+
+}

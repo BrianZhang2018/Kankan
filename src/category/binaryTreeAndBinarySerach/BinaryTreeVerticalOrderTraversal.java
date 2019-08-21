a6948a0612299a5562f562761bc7a7ada96fe895
brianzhang <willwillzhang@gmail.com>
2019-08-20 22:25:29 -0400

    update

---
+++ src/category/binaryTreeAndBinarySerach/BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,55 @@
+package category.binaryTreeAndBinarySerach;
+
+import java.util.List;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
+/**
+ * https://www.lintcode.com/problem/binary-tree-vertical-order-traversal/
+ * 
+ * 对于一棵树，我们设其根节点的位置为0
+ * For each node, its left child's degree is -1 and is right child's degree is +1. 
+ * We can do a level order traversal and save the degree information.
+ */
+public class BinaryTreeVerticalOrderTraversal{
+
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
+        helper(root, map);
+        return new ArrayList<List<Integer>>(map.values());
+    }
+     
+    private void helper(TreeNode t, TreeMap<Integer, ArrayList<Integer>> map) {
+        if (t == null) {
+            return;
+        }
+     
+        LinkedList<TreeNode> q1 = new LinkedList<>();
+        LinkedList<Integer> q2 = new LinkedList<>();
+        q1.offer(t);
+        q2.offer(0);
+     
+        while (!q1.isEmpty()) {
+            TreeNode node = q1.poll();
+            int order = q2.poll();
+     
+            //add to map
+            ArrayList<Integer> list = map.get(order);
+            if (list == null) {
+                list = new ArrayList<>();
+                map.put(order, list);
+            }
+            list.add(node.val);
+     
+            if (node.left != null) {
+                q1.offer(node.left);
+                q2.offer(order - 1);
+            }
+     
+            if (node.right != null) {
+                q1.offer(node.right);
+                q2.offer(order + 1);
+            }
+        }
+    }
+}

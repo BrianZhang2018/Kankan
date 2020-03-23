9d725215cbcdf7dacc0c5cd78be80b9d7fe4954e
brianzhang <willwillzhang@gmail.com>
2020-03-22 21:33:34 -0400

    rally healthy OA

--- src/companies/RallyHealthy/FlightReservationFor4MembersFamily.java
+++ src/companies/RallyHealthy/FlightReservationFor4MembersFamily.java
@@ -1,7 +1,36 @@
 package companies.RallyHealthy;
 
+import java.util.*;
+
 /**
  * Created by brianzhang on 3/22/20.
  */
 public class FlightReservationFor4MembersFamily {
+
+    public static void main(String[] args) {
+        System.out.println(solution(2, "1A 2F 1G"));
+    }
+
+    public static int solution(int N, String S) { // "S" is all reserved seats
+        // write your code in Java SE 8
+        if(S == null || S.length() == 0)
+            return 2*N;
+
+        String[] str = S.split(" ");
+        List<String> oc = new ArrayList<>(Arrays.asList(str));
+        int res = 0;
+        for(int i=1; i<=N; i++){
+            if(!oc.contains(i+"B") && !oc.contains(i+"C") && !oc.contains(i+"D") && !oc.contains(i+"E") && !oc.contains(i+"F") && !oc.contains(i+"G") && !oc.contains(i+"H") &&!oc.contains(i+"J")){
+                res += 2;
+            }else if(!oc.contains(i+"B") && !oc.contains(i+"C") && !oc.contains(i+"D") && !oc.contains(i+"E")){
+                res += 1;
+            }else if(!oc.contains(i+"F") && !oc.contains(i+"G") && !oc.contains(i+"H") &&!oc.contains(i+"J")){
+                res += 1;
+            }else if(!oc.contains(i+"D") && !oc.contains(i+"E") && !oc.contains(i+"F") && !oc.contains(i+"G")){
+                res += 1;
+            }
+        }
+
+        return res;
+    }
 }

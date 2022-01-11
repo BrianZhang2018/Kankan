84b294041aeaf9676a8b5cd750a5977f2e19f977
brian zhang <brianzhang@brians-MBP.fios-router.home>
2022-01-11 12:31:32 -0500

    review sys design

---
+++ src/category/Array/SpiralMatrix.java
@@ -0,0 +1,50 @@
+package category.Array;
+
+import java.util.*;
+
+// https://leetcode.com/problems/spiral-matrix/
+public class SpiralMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.toString(spiralOrder(matrix).toArray()));
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0) return result;
+
+        int startRow = 0, endRow = matrix.length-1;
+        int startCol = 0, endCol = matrix[0].length - 1;
+        int dir = 0;
+
+        while(startRow <= endRow && startCol <= endCol) {
+            switch(dir) {
+                case 0: //RIGHT
+                    for(int col = startCol; col <= endCol; col++)
+                        result.add(matrix[startRow][col]);
+                    startRow++;
+                    break;
+                case 1: //DOWN
+                    for(int row = startRow; row <=endRow; row++)
+                        result.add(matrix[row][endCol])    ;
+                    endCol--;
+                    break;
+                case 2://LEFT
+                    for(int col = endCol; col >= startCol; col --)
+                        result.add(matrix[endRow][col]);
+                    endRow--;
+                    break;
+                case 3://UP
+                    for(int row = endRow; row >= startRow; row--)
+                        result.add(matrix[row][startCol]);
+                    startCol++;
+                    break;
+            }
+            dir = (dir+1)%4;
+        }
+
+        return result;
+    }
+
+}

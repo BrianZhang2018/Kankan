2d136f6ef11f1f35b119eb978941f37d5c927dfd
brian zhang <brianzhang@brians-MBP.fios-router.home>
2022-12-04 13:48:31 -0500

    update spiral matrix - shirt the boundary inwardly after each loop

--- src/category/Array/SpiralMatrix.java
+++ src/category/Array/SpiralMatrix.java
@@ -1,50 +1,58 @@
 package category.Array;
 
 import java.util.*;
+
 /**
  * https://leetcode.com/problems/spiral-matrix/
- * 1. boudary
+ * 1. edge boudary - shift the boudary inwardly after each edge traverse
  * 2. direction
  */
 public class SpiralMatrix {
     public static void main(String[] args) {
-        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         System.out.println(Arrays.toString(spiralOrder(matrix).toArray()));
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if(matrix == null || matrix.length == 0) return result;
-
-        int startRow = 0, endRow = matrix.length-1;
-        int startCol = 0, endCol = matrix[0].length - 1;
+        List<Integer> list = new ArrayList<>();
+        // four sides' boundary
+        int row = 0, lastRow = matrix.length - 1;
+        int col = 0, lastCol = matrix[0].length - 1;
         int dir = 0;
-        while(startRow <= endRow && startCol <= endCol) {
-            switch(dir) {
-                case 0: // RIGHT
-                    for(int col = startCol; col <= endCol; col++)
-                        result.add(matrix[startRow][col]);
-                    startRow++;
+        while (row <= lastRow && col <= lastCol) {
+            switch (dir) {
+                case 0: // right
+                    for (int i = col; i <= lastCol; i++) {
+                        list.add(matrix[row][i]);
+                    }
+                    row++; // shift the boudary inwardly by 1
                     break;
-                case 1: // DOWN
-                    for(int row = startRow; row <= endRow; row++)
-                        result.add(matrix[row][endCol])    ;
-                    endCol--;
+                case 1: // down
+                    for (int i = row; i <= lastRow; i++) {
+                        list.add(matrix[i][lastCol]);
+
+                    }
+                    lastCol--;
                     break;
-                case 2: // LEFT
-                    for(int col = endCol; col >= startCol; col--)
-                        result.add(matrix[endRow][col]);
-                    endRow--;
+                case 2: // left
+                    for (int i = lastCol; i >= col; i--) {
+                        list.add(matrix[lastRow][i]);
+                    }
+                    lastRow--;
                     break;
-                case 3: // UP
-                    for(int row = endRow; row >= startRow; row--)
-                        result.add(matrix[row][startCol]);
-                    startCol++;
+                case 3: // up
+                    for (int i = lastRow; i >= row; i--) {
+                        list.add(matrix[i][col]);
+                    }
+                    col++;
                     break;
             }
-            dir = (dir+1)%4; // direction
+
+            dir = (dir + 1) % 4;
+
         }
 
-        return result;
+        return list;
+
     }
 }
